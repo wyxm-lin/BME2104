e6d6dfb9c34f5cc71dc3b7222a86dbf1f76be470
ShlQlu <2120451592@qq.com>
2024-03-09 19:14:16 +0800

    add maps and aStarEpsilon

--- searchPath.cpp
+++ searchPath.cpp
@@ -66,3 +66,57 @@ void astar(Robot &robot, Atlas& atlas) {
     robot.path = path;
     robot.pathIndex = 0;
 }
+
+void astarEpsilon(Robot &robot, Atlas& atlas, double epsilon) {
+    if (!robot.IsWorking) {
+        return;
+    }
+
+    pair<int, int> fa[MapSize + 5][MapSize + 5];
+    for (int i = 0; i < MapSize; i++) {
+        for (int j = 0; j < MapSize; j++) {
+            fa[i][j] = {-1, -1};
+        }
+    }
+
+    priority_queue <Node> openList;
+    int startX = robot.nowx, startY = robot.nowy;
+    int targetX = robot.targetX, targetY = robot.targetY;
+    Node start = Node(startX, startY, 0, epsilon * (abs(startX - targetX) + abs(startY - targetY)));
+    openList.push(start);
+    fa[startX][startY] = {startX, startY};
+
+    while (!openList.empty()) {
+        Node now = openList.top();
+        openList.pop();
+        int x = now.x, y = now.y;
+        if (x == targetX && y == targetY) { // find the target
+            break;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i], nextY = y + dy[i];
+            if (valid(nextX, nextY) == false) { // out of range
+                continue;
+            }
+            if (reachable(atlas.atlas[nextX][nextY], atlas.atlas[x][y]) == false) {  // not reachable
+                continue;
+            }
+            if (fa[nextX][nextY] != make_pair(-1, -1)) { // has been pushed into the openList
+                continue;
+            }
+            fa[nextX][nextY] = make_pair(x, y);
+            Node use4Search = Node(nextX, nextY, now.g + 1, epsilon * (abs(nextX - targetX) + abs(nextY - targetY)));
+            openList.push(use4Search);
+        }
+    }
+
+    vector<pair<int, int>> path;
+    pair<int, int> elem = {targetX, targetY}; // add the target point into path, so path = (Start, Target]
+    while (elem != make_pair(startX, startY)) {
+        path.push_back(elem);
+        elem = fa[elem.first][elem.second];
+    }
+    reverse(path.begin(), path.end());
+    robot.path = path;
+}
